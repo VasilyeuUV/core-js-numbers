b21491641f6d41e1c2fa104365c0d60ea99a41d5
VasilyeuUV <vasilyeu.uv@mail.ru>
2023-12-24 14:59:08 +0300

    feat: solve toFixed

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -352,7 +352,7 @@ function numberToStringInBase(number, base) {
  * 12345, 2    => '1.23e+4'
  */
 function toExponential(number, fractionDigits) {
-  return number.toExponential(fractionDigits);
+  return +number.toExponential(fractionDigits);
 }
 
 /**
@@ -366,8 +366,8 @@ function toExponential(number, fractionDigits) {
  * 12345, 2    => '12345.00'
  * 12.345, 1   => '12.3'
  */
-function toFixed(/* number, fractionDigits */) {
-  throw new Error('Not implemented');
+function toFixed(number, fractionDigits) {
+  return +number.toFixed(fractionDigits);
 }
 
 /**
